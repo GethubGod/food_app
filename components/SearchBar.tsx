7b4e9ab8b47f3a636d2153afe131aeac0781754f
David Chen <dontusemyemailathotmail.com@gmail.com>
2026-01-18 17:05:02 -0800

    search functionality

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,10 +1,39 @@
-import {View, Text} from 'react-native'
-import React from 'react'
+import {View, Text, TextInput, TouchableOpacity, Image} from 'react-native'
+import React, {useState} from 'react'
+import {router, useLocalSearchParams} from "expo-router";
+import {images} from "@/constants";
+import {useDebouncedCallback} from "use-debounce";
 
 const SearchBar = () => {
+    const params = useLocalSearchParams<{query?: string}>()
+    const [query, setQuery] = useState(params.query);
+
+    const debouncedSearch = useDebouncedCallback(
+        (text: string) => router.push(`/search?query=${text}`),
+        500
+    )
+
+    const handleSearch = (text: string) => {
+        setQuery(text);
+        debouncedSearch(text);
+    }
     return (
-        <View>
-            <Text>SearchBar</Text>
+        <View className="searchbar">
+            <TextInput
+                className="flex-1 p-5"
+                placeholder="Search for pizzas, burgers ..."
+                value={query}
+                onChangeText={handleSearch}
+                placeholderTextColor="#A0A0A0"
+            />
+            <TouchableOpacity className="pr-5" onPress={()=> console.log('Search Pressed')}>
+                <Image
+                    source={images.search}
+                    className="size-6"
+                    resizeMode="contain"
+                    tintColor="#5D5F6D"
+                    />
+            </TouchableOpacity>
         </View>
     )
 }
